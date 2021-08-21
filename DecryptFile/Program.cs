f0f3f0511dc60e36da6cb0c01cc94659b0f65b98
Jaime Still <jpstill85@gmail.com>
2021-08-20 23:58:58 -0400

    EncryptFile + DecryptFile + Crypto.Core + Updated

---
+++ DecryptFile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+using Crypto.Core;
+
+namespace DecryptFile
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var path = args.Length > 0
+                ? args[0]
+                : GetPath();
+
+            while (!File.Exists(path))
+            {
+                Console.WriteLine($"{path} is not a valid file path\n");
+                path = GetPath();
+            }
+
+            var file = new FileInfo(path);
+            Console.WriteLine($"Decrypting {file.FullName}...");
+
+            using var rsa = CryptoExtensions.RetrieveRsa();
+            var aesConfig = await rsa.GetAesConfig();
+
+            using var aes = Aes.Create();
+            aes.IV = aesConfig.iv;
+            aes.Key = aesConfig.key;
+
+            var decryptedFile = await aes.DecryptFile(rsa, file);
+            Console.WriteLine($"File decrypted at {decryptedFile.FullName}.");
+        }
+
+        static string GetPath()
+        {
+            Console.WriteLine("File to decrypt:");
+            return Console.ReadLine();
+        }
+    }
+}
